12d3efb5d0889509a8e66fa7127667d75eecd739
maxime.tarel <maxime.tarel@outlook.fr>
2022-05-11 13:50:55 +0200

    factorisation rapide du code reseau

--- app/src/main/java/com/example/workshop/MainActivity.kt
+++ app/src/main/java/com/example/workshop/MainActivity.kt
@@ -7,28 +7,57 @@ import com.github.kittinunf.result.Result
 import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+
+    private data class  TypeInfo (
+        val name: String,
+        val url: String,
+            )
+
+    private data class  PokemonType (
+        val type: TypeInfo
+            )
+
+    private data class  PokemonSprites (
+        val front_default: String,
+            )
+
+    private  data class PokemonInfo (
+        val id: Int,
+        val name: String,
+        val sprites: PokemonSprites,
+        val types: List<PokemonType>,
+            )
+
+    private data class Pokemon (
+        val name: String,
+        val url: String,
+        var info: PokemonInfo,
+    )
+
+    private data class PokemonList (
+        val results: List<Pokemon>
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var pokemonlist: PokemonList
+        loadPokemonList(151) {
+            pokemonlist = it
+            for (i in 0 until 151) {
+                println(pokemonlist.results[i].info.name)
+                println(pokemonlist.results[i].info.id)
+                println(pokemonlist.results[i].info.types[0].type.name)
+                println("--------")
+            }
+        }
+    }
 
-        //Ici, on crée deux data class qui vont matcher la structure des éléments que l'on souhaite récupérer dans le JSON.
-        //Il est donc important que l'on nomme les éléments de notre data class de la même manière qu'ils sont nommés dans le JSON.
-        data class Pokemon (
-            val name: String,
-            val url: String,
-                )
+    private fun loadPokemonList (limit: Int, cb: (PokemonList) -> Unit) {
 
-        data class RequestResult (
-            val results: List<Pokemon>
-                )
-
-        val limit = 5
-        
         val httpAsync = "https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0"
             .httpGet()
-            .responseString { request, response, result ->
-                println(request)
-                println(response)
+            .responseString { _, _, result ->
                 when (result) {
                     is Result.Failure -> {
                         val ex = result.getException()
@@ -36,15 +65,38 @@ class MainActivity : AppCompatActivity() {
                     }
                     is Result.Success -> {
                         val data = result.get()
-                        val ParsedJson = Gson().fromJson<RequestResult>(data, RequestResult::class.java)
+                        val parsedJson = Gson().fromJson<PokemonList>(data, PokemonList::class.java)
                         for (i in 0 until limit) {
-                            println("My Pokemon name is ${ParsedJson.results[i].name}")
-                            println("My Pokemon URL is ${ParsedJson.results[i].url}")
+                            loadPokemonProfile(parsedJson.results[i].url) {
+                                parsedJson.results[i].info = it
+                                if (parsedJson.results[i].info.id == limit) {
+                                    cb(parsedJson)
+                                }
+                            }
                         }
                     }
                 }
             }
+        httpAsync.join()
+    }
 
+    private fun loadPokemonProfile (url: String, cb: (PokemonInfo) -> Unit) {
+
+        val httpAsync = url
+            .httpGet()
+            .responseString { _, _, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        println(ex)
+                    }
+                    is Result.Success -> {
+                        val data = result.get()
+                        val pokemon = Gson().fromJson<PokemonInfo>(data, PokemonInfo::class.java)
+                        cb(pokemon)
+                    }
+                }
+            }
         httpAsync.join()
     }
 }
